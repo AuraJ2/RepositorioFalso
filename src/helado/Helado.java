d05cf70d9e654036269c61e59677c1e5b2f0d63a
DellAJ <aurajp@micorreo.upp.edu.mx>
2023-04-20 18:04:10 -0600

    FrmVenta primerdos

---
+++ src/helado/Helado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helado;
+
+/**
+ *
+ * @author DELL
+ */
+public class Helado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
